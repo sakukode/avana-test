0e19b0500da10b6578ab1a61c92ef313e18036d8
Rizqi Maulana <rizqimaulana.1988@gmail.com>
2020-01-05 06:40:24 +0700

    Change file for example

--- test2/index.php
+++ test2/index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ .'/vendor/autoload.php';
 use Avana\ExcelValidation\ExcelValidation;
 
-$file = new ExcelValidation('samples/Type_B');
+$file = new ExcelValidation('samples/Type_A');
 $output = $file->output();
 
 echo json_encode($output);
